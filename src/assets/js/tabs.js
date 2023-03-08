cecb64dd5dad2acb10f3f73749ff9f18fe825dd6
Dmitry <nickbox99@inbox.ru>
2023-03-08 23:31:15 +0400

    add ;

--- src/assets/js/tabs.js
+++ src/assets/js/tabs.js
@@ -4,25 +4,25 @@ document.addEventListener('DOMContentLoaded', function () {
       .forEach((node) => (node.style.display = display))
   }
 
-  const isVertical = window.innerWidth <= 470
+  const isVertical = window.innerWidth <= 470;
 
   document.querySelectorAll('.tabs').forEach((tabContainer) => {
-    const tabItems = [...tabContainer.querySelectorAll('.tab')]
-    const floatr = tabContainer.querySelector('.tabs__floatr')
+    const tabItems = [...tabContainer.querySelectorAll('.tab')];
+    const floatr = tabContainer.querySelector('.tabs__floatr');
 
     tabContainer.addEventListener('click', ({ target }) => {
       if (!target.classList.contains('active') && target.classList.contains('tab')) {
         tabItems.forEach((tab) => {
-          tab.classList.remove('active')
-          setVisibleTab(tab, 'none')
+          tab.classList.remove('active');
+          setVisibleTab(tab, 'none');
         })
 
-        target.classList.add('active')
-        setVisibleTab(target, '')
+        target.classList.add('active');
+        setVisibleTab(target, '');
 
-        const index = tabItems.findIndex(tab => tab.classList.contains('active'))
-        const { width, height } = target.getBoundingClientRect()
-        const transform = isVertical ? `translateY(${index * height}px)` : `translateX(${index * width}px)`
+        const index = tabItems.findIndex(tab => tab.classList.contains('active'));
+        const { width, height } = target.getBoundingClientRect();
+        const transform = isVertical ? `translateY(${index * height}px)` : `translateX(${index * width}px)`;
 
         floatr.style.cssText = `
           width: ${width}px;
