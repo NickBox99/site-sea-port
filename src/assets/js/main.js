579f14b95749aab0782a6873d6fda034daca6cca
Сергей <serj.elessar@gmail.com>
2023-03-09 21:12:27 +0300

    fix: исправил ф-ю для показа путей у карты

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,53 +1,52 @@
 const appHeight = () => {
-    const doc = document.documentElement;
-    doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+  const doc = document.documentElement
+  doc.style.setProperty('--app-height', `${window.innerHeight}px`)
 }
-window.addEventListener('resize', appHeight);
-appHeight();
+window.addEventListener('resize', appHeight)
+appHeight()
 
-window.changeVisibleRouteMap = function (btn) {
-    if(btn.classList.contains('active')) {
-        return;
-    }
-    
-    let idxSelected = -1;
-    btn.closest('.route-map__list').querySelectorAll('.route-map__route').forEach((el, idx) => {
-        if(el !== btn) {
-            el.classList.remove('active');
-        }
-        else {
-            btn.classList.add('active');
-            idxSelected = idx;
-        }
-    });
+window.changeVisibleRouteMap = function (button) {
+  let indexSelected = 1
 
-    const maps = btn.closest('.route-map').querySelectorAll('.route-map__path');
-    
-    if (idxSelected < 0 && maps[idxSelected]) {
-        return;
-    }
-    
-    maps.forEach(map => {
-        map.classList.add('hide');
-    });
-    maps[idxSelected].classList.remove('hide');
+  if (button.classList.contains('active')) {
+    return
+  }
+
+  button
+    .closest('.route-map__list')
+    .querySelectorAll('.route-map__route')
+    .forEach((route, index) => {
+      if (route !== button) {
+        route.classList.remove('active')
+      } else {
+        button.classList.add('active')
+        indexSelected = index + 1
+      }
+    })
+  
+  const mapContainer = button.closest('.route-map')
+  const activeRoutes = mapContainer.querySelectorAll(`[data-id].active`)
+  const mapRoutes = mapContainer.querySelectorAll(`[data-id="${indexSelected}"]`)
+
+  activeRoutes.forEach((route) => route.classList.remove('active'))
+  mapRoutes.forEach((route) => route.classList.add('active'))
 }
 
-document.querySelectorAll('table').forEach(table => {
-    if (!table.closest('.container')) {
-        return;
-    }
-    
-    table.removeAttribute('border');
-    table.removeAttribute('cellpadding');
-    table.removeAttribute('cellspacing');
+document.querySelectorAll('table').forEach((table) => {
+  if (!table.closest('.container')) {
+    return
+  }
 
-    if (table.parentNode.classList.contains('table-wrapper')) {
-        return;
-    }
-    
-    const wrapper = document.createElement('div');
-    wrapper.setAttribute('class', 'table-wrapper');
-    table.parentNode.insertBefore(wrapper, table);
-    wrapper.appendChild(table);
+  table.removeAttribute('border')
+  table.removeAttribute('cellpadding')
+  table.removeAttribute('cellspacing')
+
+  if (table.parentNode.classList.contains('table-wrapper')) {
+    return
+  }
+
+  const wrapper = document.createElement('div')
+  wrapper.setAttribute('class', 'table-wrapper')
+  table.parentNode.insertBefore(wrapper, table)
+  wrapper.appendChild(table)
 })
