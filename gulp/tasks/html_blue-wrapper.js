1a72dd69eead7817397aa72a8e4eb4e93065d4f0
Dmitry <nickbox99@inbox.ru>
2023-03-10 20:36:20 +0400

    init

--- gulp/tasks/html_blue-wrapper.js
+++ gulp/tasks/html_blue-wrapper.js
@@ -3,26 +3,9 @@ import webpHtml from 'gulp-webp-html-nosvg'
 import versionNumber from 'gulp-version-number'
 import htmlMin from 'gulp-htmlmin'
 
-export const html_blue = () => {
+export const html_blue_wrapper = () => {
   return app.gulp.src([
       'src/pages/edo.html',
-          'src/pages/press-service.html',
-          'src/pages/purchases.html',
-          'src/pages/contacts.html',
-          'src/pages/request-speeches.html',
-          'src/pages/fleet-processing.html',
-          'src/pages/request-pass.html',
-          'src/pages/container-trains.html',
-          'src/pages/port-client.html',
-          'src/pages/port-services.html',
-          'src/pages/vacancies.html',
-          'src/pages/educational-center.html',
-          'src/pages/students.html',
-          'src/pages/404.html',
-          'src/pages/career.html',
-          'src/pages/about.html',
-          'src/pages/search.html',
-          'src/pages/dangerous-goods.html',
           'src/pages/service-template.html'
       ])
     .pipe(app.plugins.plumber(
@@ -31,7 +14,7 @@ export const html_blue = () => {
         message: 'Error: <%= error.message %>'
       })
     ))
-    .pipe(app.plugins.wrap({src: 'src/layouts/default.html'}))
+    .pipe(app.plugins.wrap({src: 'src/layouts/default-wrapper.html'}))
     .pipe(fileinclude())
     .pipe(app.plugins.replace(/@img\//g, 'images/'))
     .pipe(app.plugins.if(app.isBuild, versionNumber({
