218d02873872c654845a627b890da8582069549a
Dmitry <nickbox99@inbox.ru>
2023-03-14 13:19:53 +0400

    move script

--- src/assets/js/animations.js
+++ src/assets/js/animations.js
@@ -1,66 +1,8 @@
-﻿if (document.querySelector('.plan-item')) {
-  const defaultOdometerConfig = {
-    value: 0,
-    format: '( ddd)',
-    theme: 'default',
-  }
+﻿document.querySelectorAll('.animate-text-top').forEach((el) => {
+  const content = el.innerHTML;
 
-  const railCounter = new Odometer({
-    el: document.querySelector('.rail-counter'),
-    ...defaultOdometerConfig,
-  })
-
-  const shipCounter = new Odometer({
-    el: document.querySelector('.ship-counter'),
-    ...defaultOdometerConfig,
-  })
-
-  const teuCounter = new Odometer({
-    el: document.querySelector('.teu-counter'),
-    ...defaultOdometerConfig,
-  })
-
-  railCounter.update(200000)
-  shipCounter.update(2000)
-  teuCounter.update(757000)
-
-  const aboutPageAnimation = () => {
-    const aboutHeading = document.querySelector('.about__heading')
-    const secondImage = document.querySelector('.containers-image')
-
-    const imageSecondAnimationObserver = new IntersectionObserver(([entry]) => {
-      if (entry.isIntersecting) {
-        secondImage.classList.add('animate-show-left')
-      }
-    })
-
-    const headingAnimationObserver = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          aboutHeading.classList.remove('animation-off')
-        }
-      },
-      {
-        threshold: 0.8,
-      }
-    )
-
-    if (secondImage) {
-      imageSecondAnimationObserver.observe(secondImage)
-    }
-
-    if (aboutHeading) {
-      headingAnimationObserver.observe(aboutHeading)
-    }
-  }
-
-  aboutPageAnimation()
-}
-document.querySelectorAll('.animate-text-top').forEach((el) => {
-  const content = el.innerHTML
-
-  el.classList.remove('animate-text-top')
-  el.classList.add('animate-text-vertical')
+  el.classList.remove('animate-text-top');
+  el.classList.add('animate-text-vertical');
   el.innerHTML = `
       <span style="color: transparent !important;" class="animate-text-vertical__container">
         ${content}
@@ -70,25 +12,25 @@ document.querySelectorAll('.animate-text-top').forEach((el) => {
           ${content}
         </span>
       </span>
-    `
+    `;
 })
 
 window.SVGAnimate = (mapId, color = '#025493') => {
-  const map = document.getElementById(mapId)
-  const routes = map.querySelectorAll('.svg-animate__path')
+  const map = document.getElementById(mapId);
+  const routes = map.querySelectorAll('.svg-animate__path');
 
   routes.forEach((path) => {
-    const pathMask = path.cloneNode()
-    const pathLength = path.getTotalLength()
+    const pathMask = path.cloneNode();
+    const pathLength = path.getTotalLength();
 
     path.style.cssText = `
       stroke-dasharray: ${pathLength};
       stroke-dashoffset: ${pathLength};
-    `
+    `;
 
-    pathMask.style.cssText = `stroke: ${color};`
+    pathMask.style.cssText = `stroke: ${color};`;
 
-    pathMask.setAttribute('class', 'svg-animate__mask')
-    map.insertBefore(pathMask, path.nextSibling)
+    pathMask.setAttribute('class', 'svg-animate__mask');
+    map.insertBefore(pathMask, path.nextSibling);
   })
 }
