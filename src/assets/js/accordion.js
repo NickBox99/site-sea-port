35e5ef7dc16b2274cfca9c21601b4f455c31015c
Dmitry <nickbox99@inbox.ru>
2023-03-22 13:30:46 +0400

    fix

--- src/assets/js/accordion.js
+++ src/assets/js/accordion.js
@@ -1,13 +1,12 @@
 document.querySelectorAll('.accordion__header').forEach(header => {
     const parent = header.parentNode;
     const wrapper = header.nextElementSibling;
-    const wrapperHeight = wrapper.scrollHeight;
 
     header.addEventListener('click', () => {
         parent.classList.toggle('open');
         
         if (parent.classList.contains('open')) {
-            wrapper.style.maxHeight = `${wrapperHeight}px`;
+            wrapper.style.maxHeight = `${wrapper.scrollHeight}px`;
         } 
         else {
             wrapper.style.maxHeight = null;
