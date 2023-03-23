cfa622d7dcd93eb3715d1260dabb087b1063adff
Dmitry <nickbox99@inbox.ru>
2023-03-23 11:17:32 +0400

    init

---
+++ src/assets/js/table.js
@@ -0,0 +1,33 @@
+﻿document.querySelectorAll('table').forEach((table) => {
+    if (!table.closest('.container')) {
+        return;
+    }
+
+    table.removeAttribute('border');
+    table.removeAttribute('cellpadding');
+    table.removeAttribute('cellspacing');
+
+    const parent = table.parentNode;
+    if (parent.classList.contains('table-wrapper')) {
+        return;
+    }
+
+    const wrapper = document.createElement('div');
+    wrapper.setAttribute('class', 'table-wrapper');
+    parent.insertBefore(wrapper, table);
+    wrapper.appendChild(table);
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.table-wrapper').forEach((wrapper, idx) => {
+        const id = `table-wrapper-${idx}`;
+        wrapper.id = id;
+
+        const touchScroll = new TouchScroll();
+        touchScroll.init({
+            id,
+            draggable: true,
+            wait: false
+        });
+    })
+})
