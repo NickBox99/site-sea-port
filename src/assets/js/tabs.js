16db8c5e0f8b59ac8da65e3544da0ccc6492746e
Dmitry <nickbox99@inbox.ru>
2023-03-23 11:58:35 +0400

    code refactoring

--- src/assets/js/tabs.js
+++ src/assets/js/tabs.js
@@ -1,52 +1,57 @@
 function setVisibleTabContent(tab, display) {
-  document.querySelectorAll(`[data-tabs="${tab.dataset.tabsOpen}"]`)
-      .forEach((node) => (node.style.display = display));
+    document.querySelectorAll(`[data-tabs="${tab.dataset.tabsOpen}"]`)
+        .forEach((node) => (node.style.display = display));
 }
 
 window.setVisibleTab = (selector, tab) => {
-  const activeTap = selector.querySelector('.active');
+    const activeTap = selector.querySelector('.active');
 
-  if (activeTap) {
-    activeTap.classList.remove('active');
-    setVisibleTabContent(activeTap, 'none');
-  }
+    if (activeTap) {
+        activeTap.classList.remove('active');
+        setVisibleTabContent(activeTap, 'none');
+    }
 
-  tab.classList.add('active');
-  setVisibleTabContent(tab, '');
+    tab.classList.add('active');
+    setVisibleTabContent(tab, '');
 }
 
 window.initInlineTabs = (selector) => {
-  if (selector) {
-    selector.addEventListener('click', ({ target }) => {
-      if (!target.classList.contains('active') && target.hasAttribute('data-tabs-open')) {
-        setVisibleTab(selector, target);
-      }
-    });
-  }
+    if (selector) {
+        selector.addEventListener('click', ({target}) => {
+            if (!target.classList.contains('active') && target.hasAttribute('data-tabs-open')) {
+                setVisibleTab(selector, target);
+            }
+        });
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  const isVertical = window.innerWidth <= 470;
+    const isVertical = window.innerWidth <= 470;
 
-  document.querySelectorAll('.tabs').forEach((tabContainer) => {
-    const tabItems = [...tabContainer.querySelectorAll('.tab')];
-    const floatr = tabContainer.querySelector('.tabs__floatr');
+    document.querySelectorAll('.tabs').forEach((tabContainer) => {
+        const tabItems = [...tabContainer.querySelectorAll('.tab')];
+        const floatr = tabContainer.querySelector('.tabs__floatr');
+        
+        function setPositionFloatr() {
+            const index = tabItems.findIndex(tab => tab.classList.contains('active'));
+            const { width, height } = tabItems[0].getBoundingClientRect();
+            const transform = isVertical ? `translateY(${index * height}px)` : `translateX(${index * width}px)`;
 
-    tabContainer.addEventListener('click', ({ target }) => {
-      if (!target.classList.contains('active') && target.classList.contains('tab')) {
-        setVisibleTab(tabContainer, target);
+            floatr.style.cssText = `
+                  width: ${width}px;
+                  transform: ${transform};
+                  -webkit-transform: ${transform};
+                  -moz-transform: ${transform};
+                `;
+        }
 
-        const index = tabItems.findIndex(tab => tab.classList.contains('active'));
-        const { width, height } = target.getBoundingClientRect();
-        const transform = isVertical ? `translateY(${index * height}px)` : `translateX(${index * width}px)`;
+        tabContainer.addEventListener('click', ({target}) => {
+            if (!target.classList.contains('active') && target.classList.contains('tab')) {
+                setVisibleTab(tabContainer, target);
+                setPositionFloatr();
+            }
+        })
 
-        floatr.style.cssText = `
-          width: ${width}px;
-          transform: ${transform};
-          -webkit-transform: ${transform};
-          -moz-transform: ${transform};
-        `
-      }
+        window.addEventListener('resize', setPositionFloatr);
     })
-  })
 })
