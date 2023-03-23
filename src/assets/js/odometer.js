88a1ff2b219cd270f3976ecf2d1a588587b1657b
Dmitry <nickbox99@inbox.ru>
2023-03-23 14:18:05 +0400

    init

---
+++ src/assets/js/odometer.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const options = {
+        value: 0,
+        format: '( ddd)',
+        theme: 'default',
+    }
+
+    document.querySelectorAll('[data-odometer-value]').forEach(el => {
+        const odometer = new Odometer({
+            ...options,
+            el
+        });
+
+        odometer.update(+el.getAttribute('data-odometer-value'));
+    })
+})
