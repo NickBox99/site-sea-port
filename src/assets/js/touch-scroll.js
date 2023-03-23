2a84d9d2ac0019fe4751cbfd73b8dfbf2d50bb44
Dmitry <nickbox99@inbox.ru>
2023-03-23 14:55:48 +0400

    +

---
+++ src/assets/js/touch-scroll.js
@@ -0,0 +1,13 @@
+﻿window.touchScrollInit = function (selector, prefix) {
+    document.querySelectorAll(selector).forEach((el, idx) => {
+        const id = `${prefix}-${idx}`;
+        el.id = id;
+
+        const viewer = new TouchScroll();
+        viewer.init({
+            id,
+            draggable: true,
+            wait: false
+        });
+    })
+}
