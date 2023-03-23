ed6ec13fd3949aede5d9bd20f051f4191619147d
Dmitry <nickbox99@inbox.ru>
2023-03-23 09:24:50 +0400

    add spaces

--- src/assets/js/animations.js
+++ src/assets/js/animations.js
@@ -1,9 +1,9 @@
 ﻿document.querySelectorAll('.animate-text-top').forEach((el) => {
-  const content = el.innerHTML;
+    const content = el.innerHTML;
 
-  el.classList.remove('animate-text-top');
-  el.classList.add('animate-text-vertical');
-  el.innerHTML = `
+    el.classList.remove('animate-text-top');
+    el.classList.add('animate-text-vertical');
+    el.innerHTML = `
       <span style="color: transparent !important;" class="animate-text-vertical__container">
         ${content}
       </span>
@@ -16,33 +16,33 @@
 });
 
 document.querySelectorAll('.animate-text-top_scroll').forEach(el => {
-  new IntersectionObserver((entries) => {
-    entries.forEach(({ isIntersecting }) => {
-      if (isIntersecting) {
-        el.classList.remove('animate-text-top_scroll');
-      }
-    })
-  }, {
-    threshold: 0.5
-  }).observe(el.nextElementSibling);
+    new IntersectionObserver((entries) => {
+        entries.forEach(({isIntersecting}) => {
+            if (isIntersecting) {
+                el.classList.remove('animate-text-top_scroll');
+            }
+        })
+    }, {
+        threshold: 0.5
+    }).observe(el.nextElementSibling);
 })
 
 window.SVGAnimate = (mapId, color = '#025493') => {
-  const map = document.getElementById(mapId);
-  const routes = map?.querySelectorAll('.svg-animate__path');
+    const map = document.getElementById(mapId);
+    const routes = map?.querySelectorAll('.svg-animate__path');
 
-  routes?.forEach((path) => {
-    const pathMask = path.cloneNode();
-    const pathLength = path.getTotalLength();
+    routes?.forEach((path) => {
+        const pathMask = path.cloneNode();
+        const pathLength = path.getTotalLength();
 
-    path.style.cssText = `
+        path.style.cssText = `
       stroke-dasharray: ${pathLength};
       stroke-dashoffset: ${pathLength};
     `;
 
-    pathMask.style.cssText = `stroke: ${color};`;
+        pathMask.style.cssText = `stroke: ${color};`;
 
-    pathMask.setAttribute('class', 'svg-animate__mask');
-    map.insertBefore(pathMask, path.nextSibling);
-  })
+        pathMask.setAttribute('class', 'svg-animate__mask');
+        map.insertBefore(pathMask, path.nextSibling);
+    })
 }
