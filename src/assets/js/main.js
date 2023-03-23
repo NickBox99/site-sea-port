02d24db15225d884a476f159c7dc9a919d24be61
Dmitry <nickbox99@inbox.ru>
2023-03-23 11:17:41 +0400

    code refactoring

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,67 +1,8 @@
-const appHeight = () => {
+function setAppHeightVariable () {
     const doc = document.documentElement;
+    
     doc.style.setProperty('--app-height', `${window.innerHeight}px`);
 }
-window.addEventListener('resize', appHeight);
-appHeight();
 
-window.changeVisibleRouteMap = function (button) {
-    let indexSelected = 1;
-
-    if (button.classList.contains('active')) {
-        return;
-    }
-
-    button
-        .closest('.route-map__list')
-        .querySelectorAll('.route-map__route')
-        .forEach((route, index) => {
-            if (route !== button) {
-                route.classList.remove('active');
-            } else {
-                button.classList.add('active');
-                indexSelected = index + 1;
-            }
-        })
-
-    const mapContainer = button.closest('.route-map');
-    const activeRoutes = mapContainer.querySelectorAll(`[data-id].active`);
-    const mapRoutes = mapContainer.querySelectorAll(`[data-id="${indexSelected}"]`);
-
-    activeRoutes.forEach((route) => route.classList.remove('active'));
-    mapRoutes.forEach((route) => route.classList.add('active'));
-}
-
-document.querySelectorAll('table').forEach((table) => {
-    if (!table.closest('.container')) {
-        return;
-    }
-
-    table.removeAttribute('border');
-    table.removeAttribute('cellpadding');
-    table.removeAttribute('cellspacing');
-
-    const parent = table.parentNode;
-    if (parent.classList.contains('table-wrapper')) {
-        return;
-    }
-
-    const wrapper = document.createElement('div');
-    wrapper.setAttribute('class', 'table-wrapper');
-    table.parentNode.insertBefore(wrapper, table);
-    wrapper.appendChild(table);
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.table-wrapper').forEach((wrapper, idx) => {
-        const id = `table-wrapper-${idx}`;
-        wrapper.id = id;
-        
-        const touchScroll = new TouchScroll();
-        touchScroll.init({
-            id,
-            draggable: true,
-            wait: false
-        });
-    })
-})
+window.addEventListener('resize', setAppHeightVariable);
+setAppHeightVariable();
