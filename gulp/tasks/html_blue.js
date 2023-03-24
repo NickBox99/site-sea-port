a04382c0a977b718b914d7b0fedad425e1009e5e
Dmitry <46891205+NickBox99@users.noreply.github.com>
2023-03-24 19:44:11 +0000

    save temp changes

--- gulp/tasks/html_blue.js
+++ gulp/tasks/html_blue.js
@@ -22,7 +22,8 @@ export const html_blue = () => {
           'src/pages/career.html',
           'src/pages/about.html',
           'src/pages/search.html',
-          'src/pages/dangerous-goods.html'
+          'src/pages/dangerous-goods.html',
+          'src/pages/test.html'
       ])
     .pipe(app.plugins.plumber(
       app.plugins.notify.onError({
