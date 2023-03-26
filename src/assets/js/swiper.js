168b5f9d23bec59251696cb1f31659edf06e72c2
Dmitry <nickbox99@inbox.ru>
2023-03-26 19:35:36 +0400

    feat: create fixLoop

---
+++ src/assets/js/swiper.js
@@ -0,0 +1,17 @@
+﻿function fixLoopSwiper() {
+    document.querySelectorAll('.swiper-wrapper').forEach(wrapper => {
+        const { offsetWidth, scrollWidth, children } = wrapper;
+        const needWidth = offsetWidth * 2.1;
+
+        let slidersWidth = scrollWidth;
+        while (slidersWidth < needWidth) {
+            [...children].forEach(el => {
+                wrapper.append(el.cloneNode(true));
+            })
+
+            slidersWidth = wrapper.scrollWidth;
+        }
+    })
+}
+
+fixLoopSwiper();
