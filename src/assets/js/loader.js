7baabfa1a29f72d1176f300c8c6320dbf2fed109
Dmitry <nickbox99@inbox.ru>
2023-05-22 11:51:01 +0400

    feat: add loader

---
+++ src/assets/js/loader.js
@@ -0,0 +1,6 @@
+﻿const loader = document.createElement('div');
+loader.classList.add('loader');
+loader.innerHTML = '<div class="loader__circle"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>';
+
+document.body.append(loader);
+window.loader = new Popup('.loader');
